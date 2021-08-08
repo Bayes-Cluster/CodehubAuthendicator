ce8e49e5d77acaeeb6f0d4f78d1a7dc12303f586
TerenceLiu98 <terence1012@gmail.com>
2021-08-08 13:11:22 +0800

    LDAP with TOTP

---
+++ codehubauthenticator.py
@@ -0,0 +1,28 @@
+from jupyterhub.auth import Authenticator
+from tornado import gen
+from paramiko import SSHClient, AuthenticationException, AutoAddPolicy
+from traitlets import Int, Unicode
+
+
+class CodehubAuthendicator(Authenticator):
+    server_address = Unicode(
+        config=True,
+        help='Address of LDAP server'
+    )
+    server_port = Int(
+        config=True,
+        help='Port of LDAP server',
+    )
+
+    @gen.coroutine
+    def authenticate(self, handler, data):
+        with SSHClient() as ssh:
+            ssh.set_missing_host_key_policy(AutoAddPolicy())
+            try:
+                ssh.connect(self.server_address, port=self.server_port,
+                            username=data['username'],
+                            password=data['password'],
+                            password=data["totp"])
+            except AuthenticationException:
+                return
+            return data['username']
